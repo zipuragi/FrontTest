6b1b0722c551ab7c50ced26cabf949bc4ed00137
zipuragi <zipuragi@gmail.com>
2015-01-29 09:53:29 +0900

    Front Test용 Commit 테스트
    첫 Commit 잘 되는지 확인해보

---
+++ deleteList/data.js
@@ -0,0 +1,41 @@
+/**
+ * Created by zipuragi on 2015. 1. 21..
+ */
+var data=[
+  {
+    "_id": "001",
+    "menu": "짜장면",
+    "price": "4000원",
+    "quantity" : "2"
+  },
+  {
+    "_id": "002",
+    "menu": "삼선짬뽕",
+    "price": "6000원",
+    "quantity" : "1"
+  },
+  {
+    "_id": "003",
+    "menu": "볶음밥",
+    "price": "5500원",
+    "quantity" : "4"
+  },
+  {
+    "_id": "004",
+    "menu": "탕짜면",
+    "price": "7000원",
+    "quantity" : "1"
+  },
+  {
+    "_id": "005",
+    "menu": "탕수육",
+    "price": "12000원",
+    "quantity" : "1"
+  },
+  {
+    "_id": "006",
+    "menu": "울면",
+    "price": "5000원",
+    "quantity": "2"
+  }
+]
---
+++ deleteList/zipuragi.js
@@ -0,0 +1,51 @@
+$(document).ready(function(){
+  $("button[name=listRender]").click(function(){
+    $("table[name=table01] tbody tr").remove();
+
+    _.each(data,function(obj){
+
+      var $tr = $("<tr>"
+        + "<td>" + obj._id + "</td>"
+        + "<td>" + obj.menu + "</td>"
+        + "<td>" + obj.price + "</td>"
+        + "<td>" + obj.quantity + "</td>"
+        + "<td><button class='btn btn-sm btn-primary' name='btnDel'><i class='glyphicon glyphicon-trash'></i> 삭제</button></td>"
+        + "</tr>"
+      );
+
+      $tr.appendTo("table[name=table01] tbody");
+    });
+
+    $("button[name=btnDel]").click(function(){
+      $(this).parent().parent().remove();
+    });
+  });
+
+  $("button[name=save]").click(function(){
+    var rtn = {};
+
+    rtn["_id"] = $("input[name=_id]").val();
+    rtn["menu"] = $("input[name=menu]").val();
+    rtn["price"] = $("input[name=price]").val();
+    rtn["quantity"] = $("input[name=quantity]").val();
+
+    var rtn = {};
+    _.each($(".form-control"), function(obj){
+      var ctrl = $(obj);
+      rtn[ctrl.attr("name")] = ctrl.val();
+    });
+
+    var rtn = {
+      _id: $("input[name=_id]").val(),
+      menu: $("input[name=menu]").val(),
+      price: $("input[name=price]").val(),
+      quantity: $("input[name=quantity]").val()
+    };
+
+    data.push(rtn);
+
+    $("button[name=listRender]").trigger('click');
+
+  });
+})
+
